b9854d52f6a11d40aac78f1f1740e12d1c6c1c42
abdulquayyum871 <quayyummohd871@gmail.com>
2023-06-02 21:46:38 +0530

    S

--- htmlcss/hotelwebsite/bard/1.js
+++ htmlcss/hotelwebsite/bard/1.js
@@ -8,3 +8,14 @@ async function bard(){
     console.log(response.output);
 }
 bard()
+
+async function bard() {
+    const request = await fetch("https://api.bardapi.dev/chat", {
+        headers: { Authorization: "Bearer d9283611-cafc-4472-a9fa-3342c67316cf" },
+        method: "POST",
+        body: JSON.stringify({ input: "I am sad suggest some hindi songs?" }),
+    });
+    const response = await request.json();
+    console.log(response.output);
+}
+bard()
